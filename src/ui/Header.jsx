defd880c56f164a2f641de7ad51645c4a970f89e
Michael Feleke <felekemichael999@gmail.com>
2024-02-17 11:02:03 +0300

    feat:building the application layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza.co</Link>
+      <p>Michael F</p>
+    </header>
+  );
+}
+
+export default Header;
