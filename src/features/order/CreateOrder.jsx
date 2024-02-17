da14feadec919700d206756b6164c6c0bf636327
Michael Feleke <felekemichael999@gmail.com>
2024-02-17 19:52:25 +0300

    feat:posting orders using the actions

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Form, json } from "react-router-dom";
+import { Form, json, redirect } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -70,7 +71,7 @@ function CreateOrder() {
           <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
 
-        <input type="hidden" name="cart" value={cart} />
+        <input type="hidden" name="cart" value={JSON.stringify(cart)} />
 
         <div>
           <button>Order now</button>
@@ -89,6 +90,10 @@ export async function action({ request }) {
     cart: JSON.parse(data.cart),
     priority: data.priority === "on",
   };
+
+  const newOrder = await createOrder(order);
+
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
