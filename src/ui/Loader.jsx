e26f151cc5b10e6bd10d6071059585d3eba2ee27
Michael Feleke <felekemichael999@gmail.com>
2024-02-18 03:12:10 +0300

    feat:making the loader more interactive

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -2,7 +2,9 @@ import styles from "./Loader.module.css";
 
 function Loader() {
   return (
-    <div className={styles.loadingContainer}>
+    <div
+      className={`${styles.loadingContainer} absolute bg-slate-200/20 backdrop-blur-sm`}
+    >
       <div className={styles.pizza}>
         <div className={styles.slice}></div>
         <div className={styles.slice}></div>
