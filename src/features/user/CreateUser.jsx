997d789e4c45680c9f2222fc7a92cbd6cf5769b7
Michael Feleke <felekemichael999@gmail.com>
2024-02-18 22:30:50 +0300

    reading the username to the header section

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
+import { updateName } from "./userSlice";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 function CreateUser() {
   const [username, setUsername] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!username) return;
+
+    dispatch(updateName(username));
+    navigate("/menu");
   }
 
   return (
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,6 +1,10 @@
+import { useSelector } from "react-redux";
 import CreateUser from "../features/user/CreateUser";
+import Button from "../../../fast-pizza/src/ui/Button";
 
 function Home() {
+  const { username } = useSelector((state) => state.user);
+
   return (
     <div className="flex justify-around items-center mx-16   space-x-[100px] h-full ">
       <div>
@@ -11,7 +15,15 @@ function Home() {
             From our kitchen to your table.
           </span>
         </h1>
-        <CreateUser />
+        {!username ? (
+          <CreateUser />
+        ) : (
+          <div className="text-center">
+            <Button to="/menu" type="primary">
+              Continuer ordering, {username}!
+            </Button>
+          </div>
+        )}
       </div>
       <div className="flex flex-col justify-center items-center w-96 h-96 bg-yellow-400 rounded-full overflow-hidden shadow-lg">
         <img
