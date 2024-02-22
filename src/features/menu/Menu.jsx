c2c1ffeac8647cb833f1b0e2e06caa9b4393d53e
Michael Feleke <felekemichael999@gmail.com>
2024-02-23 00:34:06 +0300

    making the menu responsible for all devices

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -9,7 +9,7 @@ function Menu() {
   return (
     <div className="flex items-center flex-col pb-10 h-full">
       <MenuNav />
-      <ul className="grid grid-cols-1 gap-12 max-h-full">
+      <ul className="grid grid-cols-1 gap-12 max-h-full medium:grid-cols-2 xlarge:grid-cols-3">
         {menu.map((pizza) => (
           <MenuItem pizza={pizza} key={pizza.id} />
         ))}
